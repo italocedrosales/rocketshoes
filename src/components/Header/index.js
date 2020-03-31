0f83dfc76a41384fb25df1b468f5f869f68381e1
Italo Cedro <italo.cedro.sales@gmail.com>
2020-03-30 23:01:31 -0300

    Criando Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/images/logo.svg';
 
 export default function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  )
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 itens</span>
+        </div>
+
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
 }
