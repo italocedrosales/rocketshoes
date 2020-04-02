94465eb01b73195a384b37c38af4e38bc344d4b1
Italo Cedro <italo.cedro.sales@gmail.com>
2020-04-02 19:46:34 -0300

    Alterando Quantidade no Carrinho

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,9 +1,9 @@
-import producer from 'immer';
+import produce from 'immer';
 
 export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD':
-      return producer(state, (draft) => {
+      return produce(state, (draft) => {
         const productIndex = draft.findIndex((p) => p.id === action.product.id);
 
         if (productIndex >= 0) {
@@ -14,13 +14,27 @@ export default function cart(state = [], action) {
       });
 
     case '@cart/REMOVE':
-      return producer(state, (draft) => {
+      return produce(state, (draft) => {
         const productIndex = draft.findIndex((p) => p.id === action.id);
 
         if (productIndex >= 0) {
           draft.splice(productIndex, 1);
         }
       });
+
+    case '@cart/UPDATE_AMOUNT': {
+      if (action.amount <= 0) {
+        return state;
+      }
+
+      return produce(state, (draft) => {
+        const productIndex = draft.findIndex((p) => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount = Number(action.amount);
+        }
+      });
+    }
     default:
       return state;
   }
